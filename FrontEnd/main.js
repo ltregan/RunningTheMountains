a329655728bea03abd12072841edc6eb5e8c13a4
loic tregan <ltregan@loics-MacBook-Air.local>
2015-04-04 17:17:48 +0200

    improving lightbox

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -2,8 +2,23 @@
  * Created by ltregan on 4/4/15.
  */
 
-function toggleImageZoom(){
 
-    alert("X");
+
+function openLightBox( obj ){
+    var div = document.createElement('div');
+    div.style.width = window.innerWidth;
+    div.style.height = window.innerHeight;
+    div.style.position = "fixed";
+    div.style.left = '0px';
+    div.style.top = '0px';
+    div.classList.add("lightbox");
+//    div.innerHTML = "<img src=\""+obj.src+"\" style=\"max-width:10%;max-height:10%;transform:scale(10%);\" >";
+    div.innerHTML = "<img src=\""+obj.src+"\" >";
+    div.onclick = closeLightBox;
+    document.body.appendChild(div);
+    div.focus();
 }
 
+function closeLightBox( ){
+    this.parentNode.removeChild(this);
+}
