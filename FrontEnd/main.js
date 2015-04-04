ad24ac50f12582c1248aed572d34ab4b43c19982
loic tregan <ltregan@loics-MacBook-Air.local>
2015-04-04 17:48:43 +0200

    latest lightbox

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -5,17 +5,15 @@
 
 
 function openLightBox( obj ){
-    
+
 
     var div = document.createElement('div');
 
     div.style.position = "fixed";
     div.style.left = '0px';
     div.style.top = '0px';
-    div.style.width = window.innerWidth;
-    div.style.minWidth = window.innerWidth;
-    div.style.height = window.innerHeight;
-    div.style.minHeight = window.innerHeight;
+/*    div.style.width = window.innerWidth;
+    div.style.height = window.innerHeight; */
     div.classList.add("lightbox");
 //    div.innerHTML = "<img src=\""+obj.src+"\" style=\"max-width:10%;max-height:10%;transform:scale(10%);\" >";
     div.innerHTML = "<img src=\""+obj.src+"\" >";
