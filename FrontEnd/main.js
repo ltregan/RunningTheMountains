bd2e95dee2737e208325dec445d27a5b9041f5cd
unknown <l.tregan@gmail.com>
2015-04-14 14:26:18 +0200

    lts review 1

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -3,6 +3,26 @@
  */
 
 
+function loadArticleImages(baseId){
+    var base = $( "#"+baseId);
+    return function(e) {
+        base.find('img').each(function () {
+            var oldSrc = $(this).attr('src');
+            var newSrc = baseId + "/" + oldSrc;
+            $(this).attr('src', newSrc);
+            $(this).click( function(){
+                window.location = document.baseURI+"/"+newSrc;
+            });
+        })
+    };
+}
+
+function loadArticle(baseId, title){
+    var base = $("#" + baseId);
+    base.load(baseId +" article", loadArticleImages(baseId));
+    $(".history ol ").append("<li><a href=\""+baseId+"\">"+title+"</a></li>");
+}
+
 
 function openLightBox( obj ){
 
