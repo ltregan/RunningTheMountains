52734e11a2b26fc713522ec54c32bd1c2a0706c6
unknown <l.tregan@gmail.com>
2015-04-17 19:07:01 +0200

    HTML clean up

--- FrontEnd/article.js
+++ FrontEnd/article.js
@@ -6,6 +6,7 @@ $.getScript("../core.js", function() {
 
     core.loadImages();
 
+
 });
 
 
--- FrontEnd/core.js
+++ FrontEnd/core.js
@@ -15,6 +15,15 @@ $( function() {
              });
         })
 
+        $("a").each( function(){
+           if( $(this).attr("href").startsWith("http://") ){
+               $(this).attr("target", "_blank");
+               $(this).addClass("ext");
+           }
+
+        })
+
+
     };
 
 
